30afbf730987f2432ced31bcc4f7b4362c6448cd
Husam El <113930359+fudgeadool@users.noreply.github.com>
2022-11-02 17:03:41 +0000

    hello

--- main.py
+++ main.py
@@ -2,5 +2,23 @@ coffee_prices =[('cappucino', 1.5,),
                ('expresso',1.2),
                ('mocha',1.9)]
 
-#retrieve the prices and price from the above tuple
+#retrieve the names and price from the above tuple
 
+for coffee, price in coffee_prices:
+  print(f"coffee {coffee} and price ${price}")
+
+  # if .9 - .5 - .2:
+  #  print("mocha,1.9")
+
+
+def most_expensive_coffee(coffee_prices):
+  highest_price = 0
+  my_most_expensive_coffee = ''
+  for coffee, price in coffee_prices:
+   if price > highest_price:
+     highest_price = price
+     my_most_expensive_coffee = coffee
+   else:
+     pass
+  return(my_most_expensive_coffee,highest_price)
+print(most_expensive_coffee(coffee_prices))
